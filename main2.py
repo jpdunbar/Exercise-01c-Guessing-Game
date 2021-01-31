eefeb14e8f0447d46c9ed71f5e027c8907eb5b98
Jamie Dunbar <73550787+jpdunbar@users.noreply.github.com>
2021-01-30 23:26:51 -0500

    finished excercis

--- main2.py
+++ main2.py
@@ -1,3 +1,17 @@
 #!/usr/bin/env python3
-import sys
+import sys, random
 assert sys.version_info >= (3,9), "This script requires at least Python 3.9"
+
+number = random.randrange(1,13)
+count = 0
+guess = 13
+while guess != number:
+    count += 1
+    guess = input("Guess a number from 1 to 12: ")
+    try:
+        guess = int(guess)
+        if guess != number:
+            print("Not quite, please try again.")
+    except:
+        print("Please enter a number.")
+print("You got it and were able to guess the number in " + str(count) + " tries.")
